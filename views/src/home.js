10e45f03d8b6e93b61c3a435796a7e1c82f3d41a
Cedric AUDOUY <ceaudouy@e2r6p7.42.fr>
2020-02-27 14:20:09 +0100

    infinite scrool ok error fetch not ok

--- views/src/home.js
+++ views/src/home.js
@@ -21,41 +21,48 @@ class PutFilm extends React.Component {
 				{ this.props.film.map((elem, index) => {
 					var overview = elem.overview.substr(0, 100);
 					overview[overview.length - 1] !== '.' ? overview = overview + " ..." : overview = overview + ''; 
-					return (
-						<Card  key={ index } className="root">
-							<CardHeader
-							avatar={
-								<Avatar aria-label="recipe" className="avatar"> {/* a modifier */}
-								Vu
-								</Avatar>
-							} 
-							title={ elem.title }
-							/>
-							<img className="media" src={"http://image.tmdb.org/t/p/w185/" + elem.poster_path} alt="" />
-							<CardContent>
-								<Typography variant="body2" color="textSecondary" component="p">
-									 { overview }
-						        </Typography>
-							</CardContent> 
-							<CardActions disableSpacing> 
-							<IconButton aria-label="add to favorites">
-								<FavoriteIcon />
-							</IconButton>
-							<IconButton aria-label="play/pause">
-								<section className="portfolio-experiment">
-									<a href="#`">
-										<span className="text"><PlayArrowIcon className="play-icon" />Voir le film</span>
-										<span className="line -right"></span>
-										<span className="line -top"></span>
-										<span className="line -left"></span>
-										<span className="line -bottom"></span>
-									</a>
-								</section>
-							</IconButton>
-		
-							</CardActions>
-						</Card>
-					)
+					if (elem.overview === '')
+						overview = '';
+					if (elem.poster_path === null) {
+						return ('');
+					} else {
+						return (
+							<Card  key={ index } className="root">
+								<CardHeader
+								avatar={
+									<Avatar aria-label="recipe" className="avatar"> {/* a modifier */}
+									Vu
+									</Avatar>
+								} 
+								title={ elem.title }
+								subheader= { elem.release_date }
+								/>
+								<img className="media" src={"http://image.tmdb.org/t/p/w185/" + elem.poster_path} alt="" />
+								<CardContent>
+									<Typography variant="body2" color="textSecondary" component="p">
+										{ overview }
+									</Typography>
+								</CardContent> 
+								<CardActions disableSpacing> 
+								<IconButton aria-label="add to favorites">
+									<FavoriteIcon />
+								</IconButton>
+								<IconButton aria-label="play/pause">
+									<section className="portfolio-experiment">
+										<a href="#`">
+											<span className="text"><PlayArrowIcon className="play-icon" />Voir le film</span>
+											<span className="line -right"></span>
+											<span className="line -top"></span>
+											<span className="line -left"></span>
+											<span className="line -bottom"></span>
+										</a>
+									</section>
+								</IconButton>
+			
+								</CardActions>
+							</Card>
+						)
+					}
 				})}
 			</div>
 		)
@@ -67,68 +74,54 @@ export default class Home extends React.Component {
 		super(props);
 		this.state = {
 			film: [],
-			page: 2,
+			pageNumber: 499,
+			hasMore: true,
+			genre: '',
 		}
 		this.load = 0;
 		this.hasMore = true;
 	}
 
-	request() {
-		this.load = 1;
-		fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b936c3df071b03229069cfcbe5276410&language=fr&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`, {
-			headers: new Headers({
-				'Content-Type': 'application/json',
-			})
-		}).then((response) => {
-			return response.json();
-		}).then((parsedData) => {
-			this.setState({
-				film: parsedData.results,
-			})
-		});
-	}
+	componentDidMount() {
+		this.requestMovie();
+	}	
 
-
-	InfinitReload = (query, pageNumber) => {
-		if (this.state.film.length >= 100) {
+	requestMovie = () => {
+		const { film, pageNumber } = this.state;
+		const url = `https://api.themoviedb.org/3/discover/movie?api_key=b936c3df071b03229069cfcbe5276410&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}`
+		if (pageNumber >= 1000) {
 			this.hasMore = false;
 			return;
 		} else {
-			fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b936c3df071b03229069cfcbe5276410&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=` + pageNumber.toString(), {
+			fetch(url, {
 				headers: new Headers({
 					'Content-Type': 'application/json',
 				})
 			}).then((response) => {
+				if (!response.ok) { throw response }
 				return response.json();
-			}).then((parsedData) => {
-				console.log(this.state.film.length);
-				this.setState({
-					film: this.state.film.concat(parsedData.results),
-					page: this.state.page + 1,
-				});
-			})
+			// }).then((parsedData) => {
+			// 	console.log(parsedData);
+			// 	if (!parsedData.errors) {
+			// 		this.setState({
+			// 			film: [...film, ...parsedData.results],
+			// 			pageNumber: this.state.pageNumber + 1,
+			// 		});
+			// 	}
+			}).catch((err) => { throw err });
 		}
-}
+	}
 
 	render() {
-		if (this.load === 0) {
-			this.request();
-		}
 		return (
 			<div>
 				<React.Fragment>
       				<Container fixed>
 						<Typography component="div" className="list-film" >
 							<InfiniteScroll
-							dataLength={this.state.film.length}
-							next={this.InfinitReload("", this.state.page)}
-							hasMore={ this.hasMore }
-							loader={<h4>Loading...</h4>}
-							endMessage={
-								<p style={{ textAlign: "center" }}>
-								  <b>Yay! You have seen it all</b>
-								</p>
-							  }
+								dataLength={this.state.film.length}
+								next={this.requestMovie}
+								hasMore={this.state.hasMore}
 							>
 								<PutFilm film={ this.state.film } />
 							</InfiniteScroll>
