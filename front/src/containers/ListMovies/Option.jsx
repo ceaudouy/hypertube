a06aab8880a088228eb31364e00b373d12c61855
ceaudouy <cedric.ady@gmail.com>
2020-06-02 16:57:18 +0200

    option listPage ok

--- front/src/containers/ListMovies/Option.jsx
+++ front/src/containers/ListMovies/Option.jsx
@@ -111,34 +111,9 @@ export default function OptionMenu(setQuery, type) {
 		{value: 8, label: 'Stars ↓'},
 	]
 
-
-	const optionsGenre = [
-		{value: '', label: 'Select Genre'},
-		{value: 28, label: 'Action'},
-		{value: 12, label: 'Adventure'},
-		{value: 16, label: 'Animation'},
-		{value: 35, label: 'Comedy'},
-		{value: 80, label: 'Crime'},
-		{value: 99, label: 'Documentary'},
-		{value: 18, label: 'Drama'},
-		{value: 10751, label: 'Family'},
-		{value: 14, label: 'Fantasy'},
-		{value: 36, label: 'History'},
-		{value: 27, label: 'Horror'},
-		{value: 10402, label: 'Music'},
-		{value: 9648, label: 'Mystery'},
-		{value: 10749, label: 'Romance'},
-		{value: 878, label: 'Sciense Fiction'},
-		{value: 10770, label: 'TV Movie'},
-		{value: 53, label: 'Thriller'},
-		{value: 10752, label: 'War'},
-		{value: 37, label: 'Western'},
-	]
-
 	// setValue //
 	const genreChange = event => {
-		console.log(event.target.value)
-		setGenre(event.target.value);
+		setGenre(event.label);
 	};
 
 	const dateChange = event => {
@@ -151,7 +126,7 @@ export default function OptionMenu(setQuery, type) {
 	};
 
 	const orderChange = event => {
-		setOrder(event.target.value);
+		setOrder(event.label);
 	};
 
 	// Set query for the research //
