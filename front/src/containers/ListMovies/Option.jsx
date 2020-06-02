1450902d1ee30b11f58224b2700966b83d7be2bf
ceaudouy <cedric.ady@gmail.com>
2020-06-02 16:57:18 +0200

    option listPage ok

--- front/src/containers/ListMovies/Option.jsx
+++ front/src/containers/ListMovies/Option.jsx
@@ -112,30 +112,6 @@ export default function OptionMenu(setQuery, type) {
 		{value: 8, label: 'Stars ↓'},
 	]
 
-
-	const optionsGenre = [
-		{value: '', label: 'Select Genre'},
-		{value: 28, label: 'Action'},
-		{value: 12, label: 'Adventure'},
-		{value: 16, label: 'Animation'},
-		{value: 35, label: 'Comedy'},
-		{value: 80, label: 'Crime'},
-		{value: 99, label: 'Documentary'},
-		{value: 18, label: 'Drama'},
-		{value: 10751, label: 'Family'},
-		{value: 14, label: 'Fantasy'},
-		{value: 36, label: 'History'},
-		{value: 27, label: 'Horror'},
-		{value: 10402, label: 'Music'},
-		{value: 9648, label: 'Mystery'},
-		{value: 10749, label: 'Romance'},
-		{value: 878, label: 'Sciense Fiction'},
-		{value: 10770, label: 'TV Movie'},
-		{value: 53, label: 'Thriller'},
-		{value: 10752, label: 'War'},
-		{value: 37, label: 'Western'},
-	]
-
 	// setValue //
 	const genreChange = event => {
 		setGenre(event.value);
