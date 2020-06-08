d73194b2f1d99f5c53d930df45beae52a5d0156f
ceaudouy <cedric.ady@gmail.com>
2020-06-08 12:21:39 +0200

    tableau objet option dans un fichier + responsive listpage

--- front/src/containers/ListMovies/Option.jsx
+++ front/src/containers/ListMovies/Option.jsx
@@ -61,57 +61,6 @@ export default function OptionMenu(setQuery, type) {
 	const [vote, setVote] = React.useState('');
 	const [order, setOrder] = React.useState('');
 
-
-	const optionsGenre = [
-		{value: 28, label: 'Action'},
-		{value: 12, label: 'Adventure'},
-		{value: 16, label: 'Animation'},
-		{value: 35, label: 'Comedy'},
-		{value: 80, label: 'Crime'},
-		{value: 99, label: 'Documentary'},
-		{value: 18, label: 'Drama'},
-		{value: 10751, label: 'Family'},
-		{value: 14, label: 'Fantasy'},
-		{value: 36, label: 'History'},
-		{value: 27, label: 'Horror'},
-		{value: 10402, label: 'Music'},
-		{value: 9648, label: 'Mystery'},
-		{value: 10749, label: 'Romance'},
-		{value: 878, label: 'Sciense Fiction'},
-		{value: 10770, label: 'TV Movie'},
-		{value: 53, label: 'Thriller'},
-		{value: 10752, label: 'War'},
-		{value: 37, label: 'Western'},
-	]
-	const optionsDate = [
-		{value: 2020, label: '2020'},
-		{value: 2010, label: '2010 - 2019'},
-		{value: 2000, label: '2000 - 2009'},
-		{value: 1990, label: '1990 - 1999'},
-		{value: 1980, label: '1980 - 1989'},
-		{value: 1970, label: '1970 - 1979'},
-		{value: 1960, label: '1960 - 1969'},
-		{value: 1950, label: '1950 - 1959'},
-	]
-
-	const optionsStars = [
-		{value: 5,label: '5 stars'},
-		{value: 4,label: '4 stars'},
-		{value: 3,label: '3 stars'},
-		{value: 2,label: '2 stars'},
-		{value: 1,label: '1 star'},
-		{value: 0,label: '0 star'},
-	]
-
-	const optionsOrder = [
-		{value: 3, label: 'Popularity ↑'},
-		{value: 4, label: 'Popularity ↓'},
-		{value: 5, label: 'Date ↑'},
-		{value: 6, label: 'Date ↓'},
-		{value: 7, label: 'Stars ↑'},
-		{value: 8, label: 'Stars ↓'},
-	]
-
 	// setValue //
 	const genreChange = event => {
 		setGenre(event.value);
@@ -158,6 +107,7 @@ export default function OptionMenu(setQuery, type) {
 	}
 
 	return (
+<<<<<<< HEAD
 		<OptionContainer>
 			<OneOption>
 				<Text>Genre :</Text>
@@ -180,4 +130,28 @@ export default function OptionMenu(setQuery, type) {
 			</Button>
 		</OptionContainer>
 	)	
+=======
+	<OptionContainer>
+		<OneOption>
+			<Text>Genre :</Text>
+			<Select  onChange={ genreChange } options={ optionsGenre } />
+		</OneOption>
+		<OneOption>
+			<Text>Date :</Text>
+			<Select onChange={ dateChange } options={ optionsDate } />
+		</OneOption>
+		<OneOption>
+			<Text>Stars :</Text>
+			<Select onChange={ voteChange } options={ optionsStars } />
+		</OneOption>
+		<OneOption>
+			<Text>Order by:</Text>
+			<Select onChange={ orderChange } options={ optionsOrder } />
+		</OneOption>
+		<Button onClick={ submit }>
+				Search
+		 </Button>
+	</OptionContainer>
+)
+>>>>>>> tableau objet option dans un fichier + responsive listpage
 }
