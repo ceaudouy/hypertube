58897603a9e43f359fd1ef95c441cc3f95b86e09
ceaudouy <cedric.ady@gmail.com>
2020-06-10 19:47:35 +0200

    watch page rebuild info + select episode

--- front/src/containers/Watch/SelectEpisode.jsx
+++ front/src/containers/Watch/SelectEpisode.jsx
@@ -1,118 +1,83 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import styled from 'styled-components'
+import { COLORS } from '../../config/style'
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		'& > *': {
-			margin: theme.spacing(0, 0, 0, 5),
-			maxWidth: 120,
-		},
-	},
-	formControl: {
-		margin: theme.spacing(1),
-		maxWidth: 140,
-		minWidth: 100,
-		// colo : '#adb5bd',
-	},
-	button: {
-		color: '#adb5bd',
-	  }
-}));
+const OptionContainer = styled.div `
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: stretch;
+	margin: 10;
+	background-color: ${COLORS.GREY_LOVE};
+	margin-top: 5vw;
+	border-radius: 5px;
+`
+
+const OneOption = styled.div `
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	align-items: stretch;
+	margin-top: 10px;
+	margin-bottom: 7px;
+	margin-left: 5px;
+	margin-right: 4px;
+	width: 100px;
+`
+
+const Text = styled.div`
+	color: white;
+	font-size: 0.9em;
+	margin-left: 7%;
+	margin-bottom: 2px;
+`
+
+const Select = styled.select`
+	outline: none;
+`
 
 export default function SelectEpisode(seasons) {
-	const classes = useStyles();
-	const [saison, setSaison] = useState(0);
-	const [openSaison, setOpenSaison] = useState(false);
-	const [episode, setEpisode] = useState('');
+	const [saison, setSaison] = useState(seasons === undefined ? 0 : seasons[0].episode_count);
 	const [episodeNBR, setEpisodeNBR] = useState(0);
-	const [openEpisode, setOpenEpisode] = useState(false);
-	// console.log(seasons);
 
-	useEffect(() => {
-		if (seasons !== undefined && saison !== 0 && seasons[saison] !== undefined) {
-			setEpisodeNBR(seasons[saison].episode_count)
+	const handleChange = e => {
+		let select = e.target.value
+		if (select === '') {
+			return ;
 		}
-	}, [seasons, saison])
-	// setValue //
-	const saisonChange = event => {
-		setSaison(event.target.value);
-		// console.log(saison);
-		if (seasons[saison] !== undefined ) {
-			setEpisodeNBR(seasons[saison].episode_count);
+		setEpisodeNBR(seasons[select - 1].episode_count);
+	} 
+
+	const episode = () => {
+		var obj = []
+		for (let i = 1; i <= episodeNBR; i++) {
+			obj[i] = <option key={ i } value={ i }>episode { i } </option>
 		}
-	};
-
-	const episodeChange = event => {
-		setEpisode(event.target.value);
-	};
-
-	// Open - Close Menu //
-	const handleCloseSaison = () => {
-		setOpenSaison(false);
-	};
-
-	const handleOpenSaison = () => {
-		setOpenSaison(true);
-	};
-
-	const handleCloseEpisode = () => {
-		setOpenEpisode(false);
-	};
-
-	const handleOpenEpisode = () => {
-		setOpenEpisode(true);
-	};
-	if (seasons === undefined) {
-		return ('');
+		return obj;
 	}
-	// console.log(seasons.length)
+
 	return (
-		<div>
-			<FormControl className={classes.formControl}>
-				<InputLabel className={classes.button} id="demo-controlled-open-select-label">Seasons</InputLabel>
-				<Select className={classes.button}
-					labelId="demo-controlled-open-select-label"
-					id="demo-controlled-open-select"
-					open={openSaison}
-					onClose={handleCloseSaison}
-					onOpen={handleOpenSaison}
-					value={saison}
-					onChange={saisonChange}
-				>
-					<MenuItem value="">
-					<em>None</em>
-					</MenuItem>
-					{ seasons.map((elem, index) => {
-						// console.log(elem);
+		<OptionContainer>
+			<OneOption>
+				<Text>Seasons :</Text>
+				<Select onChange={ e => handleChange(e) }>
+					<option value="">Select season</option>
+					{ seasons && seasons.map((elem, index) => {
+						console.log(elem)
 						return (
-							<MenuItem key={ index } value={ elem.season_number }>season { elem.season_number } </MenuItem>
+							<option key={ index } value={ elem.season_number }>season { elem.season_number } </option>
 						);
 					})}
 				</Select>
-			</FormControl>
-			<FormControl className={classes.formControl}>
-				<InputLabel className={classes.button} id="demo-controlled-open-select-label">Episodes</InputLabel>
-				<Select className={classes.button}
-					labelId="demo-controlled-open-select-label"
-					id="demo-controlled-open-select"
-					open={openEpisode}
-					onClose={handleCloseEpisode}
-					onOpen={handleOpenEpisode}
-					value={episode}
-					onChange={episodeChange}
-				>
-					<MenuItem value="">
-					<em>None</em>
-					</MenuItem>
-					{[...Array(episodeNBR)].map((elem, index) =>
-						<MenuItem key={ index } value={ index + 1 }>episode { index + 1 } </MenuItem>
-					)}
+			</OneOption>
+			<OneOption>
+				<Text>Episodes :</Text>
+				<Select>
+					{ episode() }
+					
 				</Select>
-			</FormControl>
-		</div>
+			</OneOption>
+		</OptionContainer>
 	);
 }
