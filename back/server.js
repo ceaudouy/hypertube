28d03bba9df698e0ffe899140de308bf4cfc288b
Guillaume Roux <guillaumeroux123@gmail.com>
2020-06-10 23:38:11 +0200

    start torrent fetch and resolve error handling problem

--- back/server.js
+++ back/server.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { handleError, passport, db } from 'middlewares';
-import { userRouter, movieRouter } from 'routes';
-// import { fetchMovies } from 'services';
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { handleError, passport, db } from 'middlewares'
+import { userRouter, movieRouter } from 'routes'
 
 dotenv.config()
 
@@ -21,7 +20,5 @@ app.use('/user', userRouter)
 app.use('/movie', movieRouter)
 
 app.use(handleError)
-
-// fetchMovies();
 
 app.listen(3300)
