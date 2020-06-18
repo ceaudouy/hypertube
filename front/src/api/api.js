6ff44917cb9d8ddc635450ae3e1e202713663aab
nicovrg <nicolasvergne88@gmail.com>
2020-06-18 16:53:11 +0200

    change api url

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const api = axios.create({ baseURL: 'http://localhost:3300'});
-const api = axios.create({ baseURL: 'http://matchapi.guillaumerx.fr:3300'});
-// const api = axios.create({ baseURL: process.env.REACT_APP_API_URL});
+// const api = axios.create({ baseURL: 'http://matchapi.guillaumerx.fr:3300'});
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL});
 
 export default api;
