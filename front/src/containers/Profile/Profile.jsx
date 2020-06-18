4bfcff114ef2fd55b19062fc7f16aaa86aaf41d6
nicovrg <nicolasvergne88@gmail.com>
2020-06-18 18:16:45 +0200

    profile info (without picture) now working and add value to input component

--- front/src/containers/Profile/Profile.jsx
+++ front/src/containers/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import api from '../../api/api'
-import { BREAK_POINTS } from '../../config/style'
+import { COLORS, BREAK_POINTS } from '../../config/style'
 import Loader from '../../components/Loader/Loader'
 import Input from '../../components/Input/Input'
 
@@ -31,6 +31,21 @@ const AccountContainer = styled.div`
 	}
 `
 
+const SubmitButton = styled.button`
+	width: 100%;
+	color: ${COLORS.WHITE};
+	background-color: ${COLORS.PINK_FLASHY};
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	border-radius: 4px;
+	outline: none;
+	cursor: pointer;
+	:hover {
+		transform: scale(1.05);
+	}
+`
+
 function Profile() {
 	const [user, setUser] = useState(false);
 	const [fetch, setFetch] = useState(false);
@@ -46,21 +61,28 @@ function Profile() {
 		});
 	}, [])
         
-    const handleMail = () => {}
-    const handleFirstname = () => {}
-    const handleLastname = () => {}
-    const handleLogin = () => {}
+    const handleMail = (e) => {setUser({...user, email: e.target.value})}
+    const handleFirstname = (e) => {setUser({...user, firstname: e.target.value})}
+    const handleLastname = (e) => {setUser({...user, lastname: e.target.value})}
+    const handleLogin = (e) => {setUser({...user, login: e.target.value})}
 
+    const handleSubmit = () => {
+		api.post('/user/update', user)
+		.catch((err) => {
+			console.log(err);
+		});
+	}
+	
 	return (
 		fetch === true ?
 		<MainContainer>
 			<AccountContainer>
-				<Input type='email' name='email' handleChange={handleMail}>{user && user.email}</Input>
-				<Input type='text' name='firstname' handleChange={handleFirstname}>{user && user.firstname}</Input>
-				<Input type='text' name='lastname' handleChange={handleLastname}>{user && user.lastname}</Input>
-				<Input type='text' name='login' handleChange={handleLogin}>{user && user.login}</Input>
+				<Input type='email' name='email' handleChange={handleMail} value={ user ? user.email : "" }/>
+				<Input type='text' name='firstname' handleChange={handleFirstname} value={ user ? user.firstname : "" }/>
+				<Input type='text' name='lastname' handleChange={handleLastname} value={ user ? user.lastname : "" }/>
+				<Input type='text' name='login' handleChange={handleLogin} value={ user ? user.login : "" }/>
+				<SubmitButton type="submit" onClick={handleSubmit}>Update</SubmitButton>
 			</AccountContainer>
-		{console.log("Coucou", user)}
 		</MainContainer>
 		: <Loader/>
 	);
