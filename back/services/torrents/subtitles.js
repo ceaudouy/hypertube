62bb74fb3a61f3f4a3706b002a8c6c22dee0fe0c
Marine <marine@MacBook-Pro-de-Marine.local>
2020-06-18 16:26:26 +0200

    parser

---
+++ back/services/torrents/subtitles.js
@@ -0,0 +1,25 @@
+// import User from 'models'
+
+// const OS = require('opensubtitles-api');
+
+// const OpenSubtitles = new OS({
+//     useragent: '',
+//     username: '',
+//     password: '',
+//     ssl: true
+// });
+
+// OpenSubtitles.api.Login();
+
+// OpenSubtitles.upload({
+// 	path: '/home/user/video.avi',       // path to video file
+// 	subpath: '/home/user/video.srt'     // path to subtitle
+// })
+// .then(status => {
+// 	status = Object {
+// 			status: '200 OK'
+// 			data: 'http://www.opensubtitles.org/subtitles/123456' //absolute link to subtitles
+// 			seconds: '1.171'
+// 	}
+// })
+// .catch(console.error);
