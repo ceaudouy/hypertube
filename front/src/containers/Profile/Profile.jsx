5c0204aa082bbd3c4dad96a9b606ab1155a4504f
nicovrg <nicolasvergne88@gmail.com>
2020-06-19 00:51:05 +0200

    add picture to profile

--- front/src/containers/Profile/Profile.jsx
+++ front/src/containers/Profile/Profile.jsx
@@ -19,11 +19,56 @@ const MainContainer = styled.div`
 	}
 `
 
-const AccountContainer = styled.div`
+const AccountPicture = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+`
+
+const ProfileImage = styled.img`
+	@media screen and (max-width: ${BREAK_POINTS.SCREEN_XS}) {
+		max-height: 50vh;
+		max-width: 50vw;
+	}
+	@media screen and (min-width: ${BREAK_POINTS.SCREEN_XS}) {
+		max-height: 20vh;
+		max-width: 20vw;
+	}
+`
+
+const StyledInput = styled.input`
+	/* display: inline-block;
+	width: 100%;
+	margin: 8px 0;
+	padding: 12px 20px;
+	border: 1px solid;
+	border-radius: 4px;
+	box-sizing: border-box;
+	background-color: ${COLORS.PURPLE_LIGHT};
+	&::-webkit-file-upload-button {
+		visibility: hidden;
+	} */
+
+	display: none;
+
+	/* margin: 0;
+	padding: 2rem 1.5rem;
+	font: 1rem/1.5 "PT Sans", Arial, sans-serif;
+	color: #5a5a5a;
+	&:before, &:after {
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	box-sizing: border-box;
+	} */
+`
+
+const AccountInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: 5vh;
 	@media screen and (max-width: ${BREAK_POINTS.SCREEN_XS}) {
 		width: 80%;
 	}
@@ -49,6 +94,7 @@ const SubmitButton = styled.button`
 
 function Profile() {
 	const [user, setUser] = useState(false);
+	const [selectedFile, setSelectedFile] = useState();
 	const [fetch, setFetch] = useState(false);
 
 	useEffect(() => {
@@ -61,29 +107,65 @@ function Profile() {
 			console.log(err);
 		});
 	}, [])
+
+
+	const addPictureFile = (e) => {
+		if (e.target.files[0].name)
+		setSelectedFile({
+			file: e.target.files[0],
+			loaded: 0,
+		});	
+	}
+
+	const uploadPicture = () => {
+		if (selectedFile && selectedFile.file) {
+			const data = new FormData()
+			data.append('file', selectedFile.file, {})
+			api.post('/user/picture', data)
+			.then((res) => {
+				api.get('/user/')
+				.then((res) => {
+					setUser(res.data);
+					setFetch(true);
+				})
+				.catch(err => {
+					console.log(err);
+				});
+			})
+			.catch((err) => {console.log(err);})
+		}
+	}
         
     const handleMail = (e) => {setUser({...user, email: e.target.value})}
     const handleFirstname = (e) => {setUser({...user, firstname: e.target.value})}
     const handleLastname = (e) => {setUser({...user, lastname: e.target.value})}
     const handleLogin = (e) => {setUser({...user, login: e.target.value})}
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+		e.preventDefault();
 		api.post('/user/update', user)
 		.catch((err) => {
 			console.log(err);
 		});
 	}
 	
+	let inputFile = '';
+	
 	return (
 		fetch === true ?
 		<MainContainer>
-			<AccountContainer>
+			<AccountPicture>
+				<ProfileImage src={`${process.env.REACT_APP_API_URL}${user.picture}`} alt={user.login} key={user.firstname} onClick={(e) => {inputFile.click();}} />
+				<StyledInput type="file" accept="image/*" name="file" label="pic" onChange={addPictureFile} ref={input => inputFile = input}/>
+				<SubmitButton type="button" onClick={uploadPicture}>{user.picture === "image/one.jpg" ? "Upload Picture" : "Change Picture"}</SubmitButton>
+			</AccountPicture>
+			<AccountInfo>
 				<Input type='email' name='email' handleChange={handleMail} value={ user ? user.email : "" }/>
 				<Input type='text' name='firstname' handleChange={handleFirstname} value={ user ? user.firstname : "" }/>
 				<Input type='text' name='lastname' handleChange={handleLastname} value={ user ? user.lastname : "" }/>
 				<Input type='text' name='login' handleChange={handleLogin} value={ user ? user.login : "" }/>
 				<SubmitButton type="submit" onClick={handleSubmit}>Update</SubmitButton>
-			</AccountContainer>
+			</AccountInfo>
 		</MainContainer>
 		: <Loader/>
 	);
