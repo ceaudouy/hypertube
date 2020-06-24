35ec9a1480db301413c9fbbea609a41fc2f40a5c
nicovrg <nicolasvergne88@gmail.com>
2020-06-24 18:51:25 +0200

    remove useless card from homepage

--- front/src/containers/Homepage/Homepage.jsx
+++ front/src/containers/Homepage/Homepage.jsx
@@ -11,27 +11,11 @@ const MainContainer = styled.div`
 	align-items: center;
 `
 
-const Typography = styled.span`
-	color: white;
-	font-size: 1rem;
-`
-
 const StyledCanvas  = styled.canvas`
 	position: absolute;
 	z-index: -2;
 	height: 100vh;
 	width: auto;
-`
-
-const Card = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	height: 20vh;
-	width: 20vw;
-	color: white;
-	background-color: black;
 `
 
 function Homepage() {
@@ -49,11 +33,6 @@ function Homepage() {
 	return (
 		<MainContainer>
 			<StyledCanvas id="canv"/>
-			<Card>
-				<Typography>
-					{ user !== undefined && user.firstname }
-				</Typography>
-			</Card>
 		</MainContainer>
 	);
 }
